ab92cada24d07a344f52592a3bffc3609397093d
Clara Moreira <anacacamm@gmail.com>
2022-09-12 16:40:31 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>
+        You are going to London next year (2023)! ✈
+      </Text>
+
+      <Button buttonText="Have faith!" />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+interface ButtonProps {
+  buttonText: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.buttonText}>
+        {props.buttonText}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#003192',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 22,
+    padding: 30,
+  },
+  buttonText: {
+    backgroundColor: '#F52933',
+    color: '#fff',
+    padding: 15,
+  }
 });
