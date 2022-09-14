b331dfa068798d1fa5415e7c04c43d723fa6c5d6
Clara Moreira <anacacamm@gmail.com>
2022-09-14 10:55:57 -0300

    feat: create Background component

--- App.tsx
+++ App.tsx
@@ -1,48 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native'
+import { Background } from './src/components/Background'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        You are going to London next year (2023)! ✈
-      </Text>
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
 
-      <Button buttonText="Have faith!" />
-      <StatusBar style="auto" />
-    </View>
+    </Background>
   );
 }
 
-interface ButtonProps {
-  buttonText: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text style={styles.buttonText}>
-        {props.buttonText}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#003192',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 22,
-    padding: 30,
-  },
-  buttonText: {
-    backgroundColor: '#F52933',
-    color: '#fff',
-    padding: 15,
-  }
-});
