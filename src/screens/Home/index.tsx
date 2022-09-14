2f547dfc1529bcc67fe69d01d789d8cfae4db5fb
Clara Moreira <anacacamm@gmail.com>
2022-09-14 11:58:24 -0300

    feat: create GameCard component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,9 @@
 import { View, Image } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
+import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
@@ -16,6 +18,10 @@ export function Home() {
       <Heading 
         title="Encontre seu duo"
         subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <GameCard 
+        data={GAMES[0]}
       />
 
     </View>
